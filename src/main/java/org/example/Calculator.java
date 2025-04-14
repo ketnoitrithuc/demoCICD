b21915e377573727ceec1401aecf29a3eedb664d
Ketnoitrithuc <fejn003@gmail.com>
2025-04-14 17:34:33 +0700

    test fail

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Calculator {
     public Integer sum(Integer a, Integer b) {
-        return 0;
+        return a/b;
     }
 
     public Integer sub(Integer a, Integer b) {
